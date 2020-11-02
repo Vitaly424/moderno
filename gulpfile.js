33dab653d736ef32ff43375d241c2a6904be7233
senior24 <vitalij.baev.98@bk.ru>
2020-11-02 12:47:37 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const cleanCss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify-es').default;
 const imagemin = require("gulp-imagemin");
-const webp = require('gulp-webp');
+// const webp = require('gulp-webp');
 // const webpHtml = require('gulp-webp-html');
 // const ttf2woff = require('gulp-ttf2woff');
 // const ttf2woff2 = require('gulp-ttf2woff2');
@@ -23,21 +23,21 @@ const path = {
       css: projectFolder + "/assets/css/",
       js: projectFolder + "/assets/js/",
       img: projectFolder + "/assets/img/",
-      fonts: projectFolder + "/assets/fonts/",
+      fonts: projectFolder + "/assets/fonts/"
    },
    src: {
       html: [sourceFolder +"/*.html", "!" + sourceFolder + "/_*.html"],
       css: sourceFolder + "/assets/scss/style.scss",
       js: sourceFolder + "/assets/js/index.js",
       img: sourceFolder + "/assets/img/**/*.{jpg,png,svg,gif,ico,webp}",
-      fonts: sourceFolder + "/assets/fonts/**/*.{ttf, eot, woff, woff2}",
+      fonts: sourceFolder + "/assets/fonts/**/*.{woff,woff2}"
    },
    watch: {
       html: sourceFolder +"/**/*.html",
       css: sourceFolder + "/assets/scss/**/*.scss",
       js: sourceFolder + "/assets/js/**/*.js",
       img: sourceFolder + "/assets/img/**/*.{jpg,png,svg,gif,ico,webp}",
-      fonts: sourceFolder + "/assets/fonts/**/*.{ttf, eot, woff, woff2}",
+      fonts: sourceFolder + "/assets/fonts/**/*.{woff,woff2}"
    },
    clean: "./" + projectFolder + "/"
 };
@@ -81,7 +81,7 @@ function css() {
 
 function fonts() {
    return src(path.src.fonts) 
-      .pipe(dest(path.build.fonts));
+          .pipe(dest(path.build.fonts));
 }
 
 function js() {
@@ -98,11 +98,11 @@ function js() {
  
 function images() {
    return src(path.src.img)
-      .pipe(
-         webp({
-            quality: 70
-         })
-      )
+      // .pipe(
+      //    webp({
+      //       quality: 70
+      //    })
+      // )
       .pipe(dest(path.build.img))
       .pipe(src(path.src.img))
       .pipe(
